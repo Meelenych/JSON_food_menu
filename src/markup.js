5b1e7995ef2781b77e78e9de806fc3975e920903
Meelenych <80649931+Meelenych@users.noreply.github.com>
2021-09-02 21:59:35 +0300

    ready

--- src/markup.js
+++ src/markup.js
@@ -1,9 +1,8 @@
+//===================markup==========================
 import menuItems from './menu.json';
 // console.log(menuItems)
-let { name, description, image, price, ingredients } = menuItems
-
 import template from './templates/template.hbs'
-
+// console.log(template)
 
 
 const mainMenu = document.querySelector('.js-menu')
@@ -11,3 +10,45 @@ const mainMenu = document.querySelector('.js-menu')
 const menuLine = template(menuItems)
 
 mainMenu.insertAdjacentHTML('afterbegin', menuLine)
+
+//===================themes==========================
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+const body = document.querySelector('body')
+// console.log(body)
+const themeSwitch = document.querySelector('#theme-switch-toggle')
+// console.log(themeSwitch)
+
+// body.classList.add(Theme.LIGHT);
+
+themeSwitch.addEventListener('click', (e) => {
+
+    if (e.target.checked
+    ) {
+        body.classList.remove(Theme.LIGHT);
+        body.classList.add(Theme.DARK);
+        localStorage.removeItem('Theme');
+        localStorage.setItem('Theme', Theme.DARK);
+    }
+
+    else {
+        body.classList.remove(Theme.DARK);
+        body.classList.add(Theme.LIGHT);
+        localStorage.removeItem('Theme');
+        localStorage.setItem('Theme', Theme.LIGHT);
+    };
+
+})
+//===================switch binding to local storage==========================
+    const localSotrageTheme = localStorage.getItem('Theme');
+    // console.log(localSotrageTheme)
+
+if (localSotrageTheme === Theme.DARK) {
+        // body.classList.remove(Theme.LIGHT);
+        body.classList.add(Theme.DARK);
+        themeSwitch.checked = true;
+    }
+
